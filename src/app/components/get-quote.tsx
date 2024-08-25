76bc1678e7ca038168e1d057cd8d4f295924484a
Shubham Kumbhakar <shubham@gosupernova.live>
2024-08-25 17:59:38 +0530

    get quote

--- src/app/components/get-quote.tsx
+++ src/app/components/get-quote.tsx
@@ -1,17 +1,22 @@
 "use client";
 
 import { useState } from "react";
+import Heading from "./heading";
 
 const GetQuote = () => {
   const [formData, setFormData] = useState({
     name: "",
-    email: "",
+    phone: "",
     service: "",
     message: "",
   });
 
   const handleChange = (e: any) => {
     const { name, value } = e.target;
+    if (name === "phone") {
+      const isNumber = value === "" || /^\d+$/.test(value);
+      if (!isNumber || value.length > 12) return;
+    }
     setFormData((prevState) => ({
       ...prevState,
       [name]: value,
@@ -23,15 +28,13 @@ const GetQuote = () => {
     // Handle form submission here (e.g., send data to an API)
     console.log("Form submitted:", formData);
     // Reset form after submission
-    setFormData({ name: "", email: "", service: "", message: "" });
+    setFormData({ name: "", phone: "", service: "", message: "" });
   };
 
   return (
     <div className="bg-gray-100 py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-3xl mx-auto">
-        <h2 className="text-3xl font-extrabold text-gray-900 text-center mb-8">
-          Get a Quote
-        </h2>
+        <Heading title="Get a Quote" />
         <form onSubmit={handleSubmit} className="grid grid-cols-1 gap-6">
           <div>
             <label
@@ -47,24 +50,24 @@ const GetQuote = () => {
               value={formData.name}
               onChange={handleChange}
               required
-              className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+              className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 outline-none"
             />
           </div>
           <div>
             <label
-              htmlFor="email"
+              htmlFor="phone"
               className="block text-sm font-medium text-gray-700"
             >
-              Email
+              Phone number
             </label>
             <input
-              type="email"
-              name="email"
-              id="email"
-              value={formData.email}
+              type="phone"
+              name="phone"
+              id="phone"
+              value={formData.phone}
               onChange={handleChange}
               required
-              className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+              className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 outline-none"
             />
           </div>
           <div>
@@ -80,12 +83,13 @@ const GetQuote = () => {
               value={formData.service}
               onChange={handleChange}
               required
-              className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+              className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 outline-none"
             >
-              <option value="">Select a service</option>
-              <option value="web-design">Web Design</option>
-              <option value="seo">SEO</option>
-              <option value="marketing">Digital Marketing</option>
+              <option value="" disabled>
+                Select a service
+              </option>
+              <option value="wedding">Wedding Photography</option>
+              <option value="pre-wedding">Pre-wedding Photography</option>
             </select>
           </div>
           <div>
@@ -93,7 +97,7 @@ const GetQuote = () => {
               htmlFor="message"
               className="block text-sm font-medium text-gray-700"
             >
-              Message
+              Message/Feedback
             </label>
             <textarea
               name="message"
@@ -101,13 +105,14 @@ const GetQuote = () => {
               rows={4}
               value={formData.message}
               onChange={handleChange}
-              className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+              className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 outline-none"
             ></textarea>
           </div>
           <div>
             <button
               type="submit"
-              className="w-full inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              className="w-full inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white disabled:bg-orange-300 bg-orange-400"
+              disabled={!formData.name || !formData.phone || !formData.service}
             >
               Get Quote
             </button>
