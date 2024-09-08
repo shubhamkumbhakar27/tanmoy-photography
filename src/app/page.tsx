66d9dd02faaecca88bfc851cc879e780f7f1ec6d
Shubham Kumbhakar <shubham@gosupernova.live>
2024-09-08 14:00:47 +0530

    correct imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,8 @@
 import About from "./components/about";
-import Blogs from "./components/blogs";
-import FilmCategories from "./components/films";
 import Footer from "./components/footer";
 import GetQuote from "./components/get-quote";
 import { HomePage } from "./components/home";
 import Navbar from "./components/navbar";
-import PhotographyPortfolio from "./components/portfolio";
 import SignatureWork from "./components/signature-work";
 import Testimonials from "./components/testimonials";
 
@@ -16,13 +13,8 @@ export default function Home() {
       <HomePage />
       <About />
       <SignatureWork />
-      {/* <PhotographyPortfolio />
-      <FilmCategories /> */}
-
       <Testimonials />
-      {/* <Blogs /> */}
       <GetQuote />
-
       <Footer />
     </>
   );
