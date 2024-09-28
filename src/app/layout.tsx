37ec4083d7970a148ddda00057479bbef11213e8
Shubham Kumbhakar <shubham@gosupernova.live>
2024-09-29 01:53:15 +0530

    correct script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -77,6 +77,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+                <script async src="https://www.googletagmanager.com/gtag/js?id=G-D9VHD3FWN6"></script>
+                <script>
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+
+                  gtag('config', 'G-D9VHD3FWN6');
+                </script>
+              `,
+          }}
+        />
+      </head>
       <body className={`${roboto.className}`}>
         <div>{children}</div>
         <Footer />
