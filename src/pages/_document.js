5568886f6114e7df75eb44533836dc740a80e40a
Shubham Kumbhakar <shubham@gosupernova.live>
2024-09-29 01:24:22 +0530

    xyz

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,15 +4,14 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <NextScript
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-D9VHD3FWN6"
-        ></NextScript>
-        <NextScript>
-          window.dataLayer = window.dataLayer || []; function gtag()
-          {window.dataLayer.push(arguments)}
-          gtag(`js`, new Date()); gtag(`config`, `G-D9VHD3FWN6`);
-        </NextScript>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-D9VHD3FWN6"></script>
+        <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){ dataLayer.push(arguments); }
+        gtag(`js`, new Date());
+
+        gtag(`config`, `G-D9VHD3FWN6`);
+        </script>
       </Head>
       <body>
         <Main />
