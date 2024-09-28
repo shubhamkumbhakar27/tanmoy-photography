c2a5ebef8cb165881b1b5497d52f463586add543
Shubham Kumbhakar <shubham@gosupernova.live>
2024-09-29 01:26:21 +0530

    using dangerouslySetInnerHTML

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,14 +4,20 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-D9VHD3FWN6"></script>
-        <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){ dataLayer.push(arguments); }
-        gtag(`js`, new Date());
-
-        gtag(`config`, `G-D9VHD3FWN6`);
-        </script>
+        <NextScript
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-D9VHD3FWN6"
+        ></NextScript>
+        <NextScript
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){ dataLayer.push(arguments); }
+            gtag('js', new Date());
+            gtag('config', 'G-D9VHD3FWN6');
+          `,
+          }}
+        />
       </Head>
       <body>
         <Main />
